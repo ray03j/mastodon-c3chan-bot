2602d59f6a097437bdd2bbd0e6c5adf181057709
ray03j <eihouse.potato@outlook.jp>
2025-01-29 20:18:40 +0900

    feat: 最初のトゥート機能

--- main.go
+++ main.go
@@ -1,10 +1,39 @@
 package main
 
 import (
-    "fmt"
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/mattn/go-mastodon"
 )
 
 func main() {
-    fmt.Println("Hello, World!")
-}
+	config := &mastodon.Config{
+		Server:       os.Getenv("MASTODON_SERVER"),
+		ClientID:     os.Getenv("CLIENT_ID"),
+		ClientSecret: os.Getenv("CLIENT_SECRET"),
+		AccessToken:  os.Getenv("ACCESS_TOKEN"),
+	}
 
+	// Create the client
+	c := mastodon.NewClient(config)
+
+	// Post a toot
+	// finalText := "test"
+	// visibility := "public"
+
+	toot := mastodon.Toot{
+		Status:     "test",
+		Visibility: "public",
+	}
+	fmt.Println("Posting toot:", toot)
+
+	post, err := c.PostStatus(context.Background(), &toot)
+	if err != nil {
+		log.Fatalf("%#v\n", err)
+	}
+
+	fmt.Println("My new post is:", post)
+}
